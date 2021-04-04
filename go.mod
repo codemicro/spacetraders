3313f1e26da1ada50e4f226e4a64d28499220e76
Tom <tom@tdpain.net>
2021-04-04 14:21:51 +0100

    Add ship controller route calculations

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/elazarl/goproxy v0.0.0-20210110162100-a92cc753f88e // indirect
 	github.com/hashicorp/go-multierror v1.1.1
+	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/parnurzeal/gorequest v0.2.16
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
--- internal/control/shipController.go
+++ internal/control/shipController.go
@@ -1,7 +1,10 @@
 package control
 
 import (
+	"github.com/codemicro/spacetraders/internal/analysis"
 	"github.com/codemicro/spacetraders/internal/stapi"
+	"github.com/imdario/mergo"
+	"strings"
 )
 
 type ShipController struct {
@@ -19,6 +22,64 @@ func NewShipController(ship *stapi.Ship, core *Core) *ShipController {
 	return s
 }
 
+func (s *ShipController) log(format string, a ...interface{}) {
+	s.core.Log("%s: "+format+"\n", append([]interface{}{s.ship.ID[:6]}, a...)...)
+}
+
+func (s *ShipController) buyGood(good string, quantity int) error {
+	newShip, err := s.core.user.SubmitPurchaseOrder(s.ship.ID, good, quantity)
+	if err != nil {
+		return err
+	}
+	return mergo.Merge(s.ship, newShip)
+}
+
+func (s *ShipController) refuel(amount int) error {
+	return s.buyGood("FUEL", amount)
+}
+
 func (s *ShipController) Start() {
-	s.core.Log("%s: online at %s (%d,%d)\n", s.ship.ID[:6], s.ship.Location, s.ship.XCoordinate, s.ship.YCoordinate)
+	s.log("online at %s (%d,%d)", s.ship.Location, s.ship.XCoordinate, s.ship.YCoordinate)
+
+	locationsInThisSystem, err := stapi.GetSystemLocations(strings.Split(s.ship.Location, "-")[0])
+	if err != nil {
+		s.log(err.Error()) // TODO: nice error handling
+		return
+	}
+
+	currentLocation, err := stapi.GetLocationInfo(s.ship.Location)
+	if err != nil {
+		s.log(err.Error()) // TODO: nice error handling
+		return
+	}
+
+	flightDestination := analysis.PickRoute(currentLocation, locationsInThisSystem, analysis.RoutingMethodShort)
+	flightDistance := analysis.FindDistance(currentLocation, flightDestination)
+
+	journeyFuel := analysis.CalculateFuelForFlight(currentLocation, flightDestination)
+	extraFuelRequired := journeyFuel - s.ship.GetCurrentFuel()
+
+	s.log("found destination: dist %d, %#v", flightDistance, flightDestination)
+	s.log("total fuel required for journey: %d", journeyFuel)
+
+	if extraFuelRequired > 0 {
+		s.log("fuelling with %d units of fuel", extraFuelRequired)
+		err = s.refuel(extraFuelRequired)
+		if err != nil {
+			s.log(err.Error()) // TODO: nice error handling
+			return
+		}
+	} else {
+		s.log("no extra fuel required")
+	}
+
+	//goods, err := stapi.GetMarketplaceAtLocation(s.ship.Location)
+	//if err != nil {
+	//	s.log(err.Error()) // TODO: nice error handling
+	//	return
+	//}
+	//
+	//for _, good := range goods {
+	//	s.log("good %#v", good)
+	//}
 }
