16fca428402059e74fdb733ef5cc3428a9b9ecf9
Tom <tom@tdpain.net>
2021-04-04 12:48:47 +0100

    Routing: Add extra methods, filter out current location

--- internal/analysis/route.go
+++ internal/analysis/route.go
@@ -9,21 +9,30 @@ type RoutingMethod uint8
 
 const (
 	RoutingMethodShortest = iota
+	RoutingMethodShort
 	RoutingMethodMedium
 	RoutingMethodLong
+	RoutingMethodLongest
 )
 
 func PickRoute(from *stapi.Location, options []*stapi.Location, method RoutingMethod) *stapi.Location {
+
+	// remove from from options if exists
+	for i, opt := range options {
+		if opt == nil { // this will occur if anything has been deleted, since it is replaced with nil
+			continue
+		}
+		if from.Symbol == opt.Symbol {
+			options[i] = options[len(options)-1]
+			options[len(options)-1] = nil
+			options = options[:len(options)-1]
+		}
+	}
+
 	distances := make(map[string]int64)
 
 	for _, routeOption := range options {
-
-		if from.Symbol == routeOption.Symbol {
-			continue
-		}
-
 		distances[routeOption.Symbol] = FindDistance(from, routeOption)
-
 	}
 
 	sort.Slice(options, func(i, j int) bool {
@@ -37,9 +46,13 @@ func PickRoute(from *stapi.Location, options []*stapi.Location, method RoutingMe
 	switch method {
 	case RoutingMethodShortest:
 		targetLocation = options[0]
+	case RoutingMethodShort:
+		targetLocation = options[len(options) / 4]
 	case RoutingMethodMedium:
-		targetLocation = options[len(options) / 2] // will always tend towards the highest middle value
+		targetLocation = options[len(options) / 2]
 	case RoutingMethodLong:
+		targetLocation = options[(len(options) / 4) * 3]
+	case RoutingMethodLongest:
 		targetLocation = options[len(options) - 1]
 	}
 
