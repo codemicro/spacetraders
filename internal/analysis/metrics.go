6291e623461b242b40e07108997133aa6d2590a6
Tom <tom@tdpain.net>
2021-04-04 14:18:45 +0100

    Add metrics.CalculateFuelForFlight

--- internal/analysis/metrics.go
+++ internal/analysis/metrics.go
@@ -10,3 +10,22 @@ func FindDistance(from, to *stapi.Location) int64 {
 	y := float64(to.YCoordinate - from.YCoordinate)
 	return int64(math.Round(math.Sqrt(x*x + y*y)))
 }
+
+const fuelContingency = 2
+
+func CalculateFuelForFlight(from, to *stapi.Location) int {
+
+	dist := FindDistance(from, to)
+
+	var rawFuelRequired int
+	{
+		d := int(math.Round(float64(dist) / 4))
+		e := 1
+		if from.Type == stapi.LocationTypePlanet {
+			e += 2
+		}
+		rawFuelRequired = d + e
+	}
+
+	return rawFuelRequired + fuelContingency
+}
