07c0a75ddbe496d3dd0e7707ecf29f099133b926
Tom <tom@tdpain.net>
2021-04-05 11:30:20 +0100

    Analysis: add market data storage facilities
    Signed-off-by: Tom <tom@tdpain.net>

---
+++ internal/analysis/markets.go
@@ -0,0 +1,78 @@
+package analysis
+
+import (
+	"encoding/json"
+	"github.com/codemicro/spacetraders/internal/stapi"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
+)
+
+// This file is for things related to tracking the state of markets in systems
+
+type Markets map[string]*MarketInfo
+
+type MarketInfo struct {
+	Symbol string
+	Time time.Time
+	Goods []*stapi.MarketplaceGood
+}
+
+const marketTrackerFile = "markets.json"
+
+var (
+	currentMarketState = make(Markets)
+	marketTrackerLock = new(sync.RWMutex)
+)
+
+func init() {
+	// load markets file if exists
+	var fileExists bool
+	{
+		_, err := os.Stat(marketTrackerFile)
+		fileExists = err == nil
+	}
+
+	if fileExists {
+		rawData, err := ioutil.ReadFile(marketTrackerFile)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(rawData, &currentMarketState)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func RecordMarketplaceAtLocation(location string, marketplace []*stapi.MarketplaceGood) error {
+	marketTrackerLock.Lock()
+	defer marketTrackerLock.Unlock()
+
+	currentMarketState[location] = &MarketInfo{
+		Symbol: location,
+		Time:   time.Now(),
+		Goods:  marketplace,
+	}
+
+	jsonData, err := json.MarshalIndent(currentMarketState, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(marketTrackerFile, jsonData, 0644)
+}
+
+func GetMarketplaceAtLocation(location string) []*stapi.MarketplaceGood {
+	marketTrackerLock.RLock()
+	defer marketTrackerLock.RUnlock()
+
+	curr, ok := currentMarketState[location]
+	if !ok {
+		return nil
+	}
+
+	return curr.Goods
+}
