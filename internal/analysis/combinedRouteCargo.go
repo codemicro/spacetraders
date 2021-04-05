c12c203955b2984581966e9a4155901c44a768ee
Tom <tom@tdpain.net>
2021-04-05 21:25:53 +0100

    Analysis: add FindCombinedRouteAndCargo

---
+++ internal/analysis/combinedRouteCargo.go
@@ -0,0 +1,107 @@
+package analysis
+
+import (
+	"errors"
+	"github.com/codemicro/spacetraders/internal/stapi"
+	"github.com/codemicro/spacetraders/internal/tool"
+	"sort"
+	"strings"
+)
+
+type cargoDestination struct {
+	Cargo string
+	Destination string
+	Value float64
+}
+
+func FindCombinedRouteAndCargo(currentLocationSymbol string) (*stapi.Location, *stapi.MarketplaceGood, error) {
+
+	systemLocations, err := stapi.GetSystemLocations(tool.SystemFromSymbol(currentLocationSymbol))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var currentLocation *stapi.Location
+	{
+		// filter out current location and any wormholes
+		var n int
+		for _, location := range systemLocations {
+			isCurrent := strings.EqualFold(location.Symbol, currentLocationSymbol)
+			if !isCurrent && location.Type != stapi.LocationTypeWormhole {
+				systemLocations[n] = location
+				n++
+			} else if isCurrent {
+				currentLocation = location
+			}
+		}
+		systemLocations = systemLocations[:n]
+	}
+
+	distancesTo := make(map[string]int)
+	for _, location := range systemLocations {
+		distancesTo[location.Symbol] = FindDistance(currentLocation, location)
+	}
+
+	marketplaces := GetAllMarketplaces()
+	var rankings []cargoDestination
+
+	var currentLocationGoods []stapi.MarketplaceGood
+	for key, val := range marketplaces {
+		if strings.EqualFold(currentLocationSymbol, key) {
+			currentLocationGoods = val.Goods
+		}
+	}
+
+	for _, currentLocationGood := range currentLocationGoods {
+		// for every other market
+		for _, market := range marketplaces {
+			for _, marketGood := range market.Goods {
+				if strings.EqualFold(currentLocationGood.Symbol, marketGood.Symbol) {
+					// the destination also has this type of good
+
+					if currentLocationGood.PurchasePricePerUnit > marketGood.SellPricePerUnit {
+						// if we're going to lose money, we're not interested
+						continue
+					}
+
+					rankings = append(rankings, cargoDestination{
+						Cargo:       marketGood.Symbol,
+						Destination: market.Symbol,
+						Value:       float64(marketGood.SellPricePerUnit) / float64(distancesTo[market.Symbol]*marketGood.VolumePerUnit),
+					})
+
+				}
+			}
+		}
+
+	}
+
+	sort.Slice(rankings, func(i, j int) bool {
+		return rankings[i].Value > rankings[j].Value
+	})
+
+	if len(rankings) < 1 {
+		return nil, nil, errors.New("analysis: no suitable routes")
+	}
+
+	selected := rankings[0]
+
+	var selectedLocation *stapi.Location
+	for _, loc := range systemLocations {
+		if strings.EqualFold(loc.Symbol, selected.Destination) {
+			selectedLocation = loc
+			break
+		}
+	}
+
+	var selectedGood *stapi.MarketplaceGood
+	for _, good := range currentLocationGoods {
+		if strings.EqualFold(selected.Cargo, good.Symbol) {
+			selectedGood = &good
+			break
+		}
+	}
+
+	return selectedLocation, selectedGood, nil
+
+}
